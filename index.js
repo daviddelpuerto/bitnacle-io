f0576476ca764f61d8bbb35da281773a889e8069
David del Puerto <ddelpuertomartin@gmail.com>
2019-10-12 11:47:54 +0200

    Linted code

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
-'use strict';
-
 const { bitnacleTimer } = require('bitnacle-helpers');
 
-const isEventExcluded = function(eventName, options) {
-    if (!eventName) return false;
-    if (options && options.exclude && options.exclude.includes(eventName)) return true; 
-    else return false;
-};
+function isEventExcluded(eventName, options) {
+  if (!eventName) return false;
+  if (options && options.exclude && options.exclude.includes(eventName)) return true;
+  return false;
+}
 
-module.exports = function(options = {}) {
+module.exports = (options = {}) => {
+  if (options !== undefined && typeof options !== 'object') {
+    throw new Error('Invalid argument type, you must pass an object to bitnacleIo');
+  }
 
-    if (options.constructor.name !== 'Object') {
-        throw new Error('It seems you are using bitnacleIo middleware this way "app.use(binacleIo)", but it should be used "app.use(bitnacleIo())".');
+  if (options.constructor.name !== 'Object') {
+    throw new Error('It seems you are using bitnacleIo middleware this way "app.use(binacleIo)", but it should be used "app.use(bitnacleIo())".');
+  }
+
+  return (eventData, next) => {
+    if (options && options.exclude && !Array.isArray(options.exclude)) {
+      throw new Error('Invalid argument type, exclude property must be an array');
     }
 
-    if (typeof options !== 'object') {
-        throw new Error('Invalid argument type, you must pass and object');
+    const eventName = Object.values(eventData)[0];
+    if (isEventExcluded(eventName, options)) return;
+
+    const time = bitnacleTimer.getRequestTime();
+
+    if (options.format && options.format === 'json') {
+      process.stdout.write(`${JSON.stringify({ time, eventData })}\n`);
+    } else {
+      process.stdout.write(`[${time}] ${JSON.stringify(eventData)}\n`);
     }
-    
-    return function(eventData, next) {    
 
-        if (Object.entries(options).length && options.exclude && !Array.isArray(options.exclude)) {
-            throw new Error('Invalid argument type, exclude property must be an array');
-        }
-
-        const eventName = Object.values(eventData)[0];
-        if (isEventExcluded(eventName, options)) return;
-
-        const time = bitnacleTimer.getRequestTime();
-
-        if (options.format && options.format === 'json') {
-            process.stdout.write(`${JSON.stringify({ time, eventData })}\n`);
-        } else {
-            process.stdout.write(`[${time}] ${JSON.stringify(eventData)}\n`);
-        }    
-
-        next();
-
-    };
-        
+    next();
+  };
 };
